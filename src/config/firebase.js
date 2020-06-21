2db0975b6c21a029f8734e5822ab4728032cb4fc
Priyal Jain <priyal.jain@engineerbabu.in>
2020-06-21 18:13:07 +0530

    gdg indore commit updated

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,28 +1,28 @@
 import firebase from 'firebase/app';
-import 'firebase/auth';   
+import 'firebase/auth';
 import 'firebase/storage';
 import 'firebase/firestore';
 import 'firebase/messaging';
 import 'firebase/functions';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCOl0qzkhCDmyWeeIT8AZe4xyJJlIfU5gE",
-  authDomain: "myaurapp.firebaseapp.com",
-  databaseURL: "https://myaurapp.firebaseio.com",
-  projectId: "myaurapp",
-  storageBucket: "myaurapp.appspot.com",
-  messagingSenderId: "419511997060",
-  appId: "1:419511997060:web:ce7aa0d308f6b484f868d1"
+    apiKey: "AIzaSyDTiDYPJoN3vIsSMt05Tb0u0fLsKup6T9s",
+    authDomain: "gdg-indore-aura.firebaseapp.com",
+    databaseURL: "https://gdg-indore-aura.firebaseio.com",
+    projectId: "gdg-indore-aura",
+    storageBucket: "gdg-indore-aura.appspot.com",
+    messagingSenderId: "869364317165",
+    appId: "1:869364317165:web:65e868c57e78ed137cc5e4"
 };
 
 firebase.initializeApp(firebaseConfig);
-firebase.firestore().enablePersistence({synchronizeTabs:!0}).catch(()=>{console.warn("DB offline support not available")})
-export default{
-  notificationSupported:firebase.messaging.isSupported(),
-  messaging: (firebase.messaging.isSupported())?firebase.messaging():null,
-  firestore:firebase.firestore(),
-  auth:firebase.auth(),
-  authw:firebase.auth,
-  storage:firebase.storage(),
-  functions: firebase.functions()
+firebase.firestore().enablePersistence({ synchronizeTabs: !0 }).catch(() => { console.warn("DB offline support not available") })
+export default {
+    notificationSupported: firebase.messaging.isSupported(),
+    messaging: (firebase.messaging.isSupported()) ? firebase.messaging() : null,
+    firestore: firebase.firestore(),
+    auth: firebase.auth(),
+    authw: firebase.auth,
+    storage: firebase.storage(),
+    functions: firebase.functions()
 };
